0eb77ae879100e217333bdaf8ae84102100d96d0
mlbpro417 <mlbpro417@gmail.com>
2018-02-28 14:42:12 -0800

    test

--- spec/database.test.js
+++ spec/database.test.js
@@ -14,7 +14,7 @@ describe('Test querying the database', () => {
   beforeAll(() => client.connect());
   // const DIRNAME = path.resolve();
   // console.log('THIS IS DIRNAME', DIRNAME);
-  client.query('DROP TABLE images;');
+  // client.query('DROP TABLE images;');
   client.query('CREATE TABLE images (product_id int, large_image_url varchar, small_gallery_image_url varchar);');
   seedData.forEach((data) => {
     client.query('INSERT INTO images (product_id, large_image_url, small_gallery_image_url) values ($1, $2, $3);', [data.product_id, data.large_image_url, data.small_gallery_image_url]);
