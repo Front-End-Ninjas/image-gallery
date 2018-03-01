09700b7e835f5740d34a5672ad8b5710a55141f7
mlbpro417 <mlbpro417@gmail.com>
2018-02-28 16:07:03 -0800

    deleted unecessary comments

--- spec/database.test.js
+++ spec/database.test.js
@@ -1,6 +1,5 @@
 const { Client } = require('pg');
 const seedData = require('../seed_data');
-const path = require('path');
 
 let connectionString;
 if (process.env.POSTGRES_USER && process.env.POSTGRES_DB) {
@@ -12,18 +11,13 @@ const client = new Client(connectionString);
 
 describe('Test querying the database', () => {
   beforeAll(() => client.connect());
-  // const DIRNAME = path.resolve();
-  // console.log('THIS IS DIRNAME', DIRNAME);
-  // client.query('DROP TABLE images;');
   client.query('CREATE TABLE images (product_id int, large_image_url varchar, small_gallery_image_url varchar);');
   seedData.forEach((data) => {
     client.query('INSERT INTO images (product_id, large_image_url, small_gallery_image_url) values ($1, $2, $3);', [data.product_id, data.large_image_url, data.small_gallery_image_url]);
   });
-  // client.query('COPY images (product_id, large_image_url, small_gallery_image_url) FROM \'' + DIRNAME + '/seed_data.js\' WITH DELIMITER \',\';');
   afterAll(() => client.end());
 
   test('Should get an array length of 1500', () => {
-
     expect.assertions(2);
     return client.query('SELECT * FROM images')
       .then((res) => {
