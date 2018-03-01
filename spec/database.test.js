441f33fc31bc319c4fe01e252a5254af86b51e93
mlbpro417 <mlbpro417@gmail.com>
2018-02-28 17:11:37 -0800

    used forEach to iterate through data instead of COPY

--- spec/database.test.js
+++ spec/database.test.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+const seedData = require('../seed_data');
 
 let connectionString;
 if (process.env.POSTGRES_USER && process.env.POSTGRES_DB) {
@@ -6,15 +7,17 @@ if (process.env.POSTGRES_USER && process.env.POSTGRES_DB) {
 } else {
   connectionString = 'postgres://localhost/admin';
 }
-
 const client = new Client(connectionString);
 
 describe('Test querying the database', () => {
   beforeAll(() => client.connect());
-
+  client.query('CREATE TABLE images (product_id int, large_image_url varchar, small_gallery_image_url varchar);');
+  seedData.forEach((data) => {
+    client.query('INSERT INTO images (product_id, large_image_url, small_gallery_image_url) values ($1, $2, $3);', [data.product_id, data.large_image_url, data.small_gallery_image_url]);
+  });
   afterAll(() => client.end());
 
-  test('Should get an array length of 1360', () => {
+  test('Should get an array length of 1500', () => {
     expect.assertions(2);
     return client.query('SELECT * FROM images')
       .then((res) => {
@@ -29,7 +32,7 @@ describe('Test querying the database', () => {
       .then(({ rows }) => {
         expect(rows.length).toBe(5);
         expect(rows[0]).toBeInstanceOf(Object);
-        expect(rows[0].large_image_url).toBe('\'movies/url1.jpg\'');
+        expect(rows[0].large_image_url).toBe('movies/url1.jpg');
       });
   });
 
@@ -39,7 +42,7 @@ describe('Test querying the database', () => {
       .then(({ rows }) => {
         expect(rows.length).toBe(5);
         expect(rows[0]).toBeInstanceOf(Object);
-        expect(rows[0].small_gallery_image_url).toBe('\'movies/url1.jpg\'');
+        expect(rows[0].small_gallery_image_url).toBe('movies/url1.jpg');
       });
   });
 });
