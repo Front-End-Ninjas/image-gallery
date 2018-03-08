22518b2fb20984dda324cc5f48d1c280004892b4
mlbpro417 <mlbpro417@gmail.com>
2018-03-08 12:20:59 -0800

    client connection details

--- database/pgClient.js
+++ database/pgClient.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  user: process.env.POSTGRES_USER || '',
-  database: process.env.POSTGRES_DB || 'image',
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT || 5432,
+  host: process.env.POSTGRES_HOST || process.env.RDS_HOSTNAME || 'localhost',
+  user: process.env.POSTGRES_USER || process.env.RDS_USERNAME || '',
+  database: process.env.POSTGRES_DB || process.env.RDS_DB_NAME || 'image',
+  password: process.env.POSTGRES_PASSWORD || process.env.RDS_PASSWORD,
+  port: process.env.POSTGRES_PORT || process.env.RDS_PORT || 5432,
 });
 
 module.exports = client;
